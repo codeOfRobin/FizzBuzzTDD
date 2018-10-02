082c069cbc233ea26777e7e0feb2da6d08cdc02c
Robin Malhotra <me@rmalhotra.com>
2018-10-02 22:01:22 +0530

    removed storyboards because storyboards are stupid
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>
    
    Don’t @ me
    
    Signed-off-by: Robin Malhotra <me@rmalhotra.com>

--- FizzBuzz/ViewController.swift
+++ FizzBuzz/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.view.backgroundColor = .red
         // Do any additional setup after loading the view, typically from a nib.
     }
 
